b07d50fe3a024a6bdb72324ff539fe8da913088e
rinpopoyo <rinpopoyo@gmail.com>
2018-03-30 16:59:50 +0900

    first

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="section is-large">
+    <div class="container has-text-centered">
+      <h1 class="title">Hi, I'm Chimezie Enyinnaya</h1>
+      <h2 class="subtitle">a Software Developer based in Lagos, Nigeria.</h2>
+    </div>
+  </section>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
